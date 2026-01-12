9d03445722c00ae17693524c5faa9046336c0de2
Knud Mondrup <gadelamperne@gmail.com>
2026-01-12 12:10:10 +0100

    Update with RBG and startup sequence

--- Knud/phase1/main/main.c
+++ Knud/phase1/main/main.c
@@ -4,35 +4,75 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "driver/i2c.h"
+#include "driver/ledc.h"
 #include "soc/gpio_reg.h"
+#include "esp_adc/adc_oneshot.h"
 
 
 #define LED_PIN GPIO_NUM_9
-#define BUTTON_PIN GPIO_NUM_1
+#define BUTTON_PIN GPIO_NUM_4
 #define SDA_PIN GPIO_NUM_5 
 #define SCL_PIN GPIO_NUM_6
 #define AM2320_ADDR 0x5C
 #define I2C_PORT 0
+#define RED_LED GPIO_NUM_0
+#define GREEN_LED GPIO_NUM_1
+#define BLUE_LED GPIO_NUM_3
 
-volatile int led_level = 0;
+
+volatile int led_level = 1;
 volatile bool temp_humid_sensor = true;
+int program_begun = false;
+
+void red(), green(), blue(), purple(), yellow(), white(), cyan(), all_off();
 
 void interrupt_handler()
 {
-    led_level = !led_level;
-    temp_humid_sensor = !temp_humid_sensor;
+    if (program_begun) {
+        led_level = !led_level;
+        temp_humid_sensor = !temp_humid_sensor;
+    } else {
+        
+    }
+    
+}
+
+
+unsigned short crc16(unsigned char *ptr, unsigned char len)
+{
+    unsigned short crc =0xFFFF;
+    unsigned char i;
+
+    while(len--)
+    {
+        crc ^=*ptr++;
+        for(i=0;i<8;i++)
+        {
+            if(crc & 0x01)
+            {
+            crc>>=1;
+            crc^=0xA001;
+            }else
+            {
+            crc>>=1;
+            }
+        }
+    }
+    return crc;
 }
 
 void app_main(void)
 {
     gpio_config_t io_conf = {
-        .pin_bit_mask = (1ULL << LED_PIN),
+        .pin_bit_mask = (1ULL << LED_PIN) + (1ULL << RED_LED) + (1ULL << GREEN_LED) + (1ULL << BLUE_LED),
         .mode = GPIO_MODE_OUTPUT,
         .pull_up_en = GPIO_PULLUP_DISABLE,
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
         .intr_type = GPIO_INTR_DISABLE
     };
     gpio_config(&io_conf);
+
+    all_off();
 
     // Configure Button
     gpio_config_t io_conf_ins = {
@@ -61,41 +101,96 @@ void app_main(void)
     i2c_param_config(I2C_NUM_0, &i2c_conf);
     i2c_driver_install(I2C_NUM_0, i2c_conf.mode, 0, 0,0);
 
-    //wake
-    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-    i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, (AM2320_ADDR << 1) | I2C_MASTER_WRITE, true);
-    i2c_master_stop(cmd);
-    i2c_master_cmd_begin(I2C_PORT, cmd, pdMS_TO_TICKS(50));
-    i2c_cmd_link_delete(cmd);
 
+    // Konfigurerer den analoge sensor på pin 2 på microcontrolleren
+    adc_oneshot_unit_handle_t adc_handle;
 
-    //write
-    cmd = i2c_cmd_link_create();
-    i2c_master_start(cmd);
+    adc_oneshot_unit_init_cfg_t init_cfg = {
+        .unit_id = ADC_UNIT_1,
+    };
+    adc_oneshot_new_unit(&init_cfg, &adc_handle);
 
-    uint8_t req[4] =
-    {
-        (AM2320_ADDR << 1) | I2C_MASTER_WRITE,
-        0X03,
-        0X00,
-        0X04
+    adc_oneshot_chan_cfg_t chan_cfg = {
+        .bitwidth = ADC_BITWIDTH_12,
+        .atten = ADC_ATTEN_DB_11,
     };
 
-    i2c_master_write(cmd, req, sizeof(req), true);
+    adc_oneshot_config_channel(adc_handle, ADC_CHANNEL_2, &chan_cfg);
 
-    i2c_master_stop(cmd);
-    i2c_master_cmd_begin(I2C_PORT, cmd, pdMS_TO_TICKS(50));
-    i2c_cmd_link_delete(cmd);
+   
+    // Startup sequence
+    //Tænder og slukker for LED 3 gange
+    for (int i = 0; i < 3; i++){
+        gpio_set_level(LED_PIN, 1);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        gpio_set_level(LED_PIN, 0);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+
+    //Cycle through RGB colours
+    red();
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    green();
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    blue();
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    purple();
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    yellow();
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    white();
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    cyan();
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    all_off();
+    
+
+
+
+
+
+
+
+
+
+
+
+
 
     while (true) {
-
         gpio_set_level(LED_PIN, led_level);
         vTaskDelay(pdMS_TO_TICKS(10));
 
-
         if (temp_humid_sensor) {
             // Temperature and humidity readout
+
+            //wake
+            i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+            i2c_master_start(cmd);
+            i2c_master_write_byte(cmd, (AM2320_ADDR << 1) | I2C_MASTER_WRITE, true);
+            i2c_master_stop(cmd);
+            i2c_master_cmd_begin(I2C_PORT, cmd, pdMS_TO_TICKS(50));
+            i2c_cmd_link_delete(cmd);
+
+
+            //write
+            cmd = i2c_cmd_link_create();
+            i2c_master_start(cmd);
+
+            uint8_t req[4] =
+            {
+                (AM2320_ADDR << 1) | I2C_MASTER_WRITE,
+                0X03,
+                0X00,
+                0X04
+            };
+
+            i2c_master_write(cmd, req, sizeof(req), true);
+
+            i2c_master_stop(cmd);
+            i2c_master_cmd_begin(I2C_PORT, cmd, pdMS_TO_TICKS(50));
+            i2c_cmd_link_delete(cmd);
+
 
             //read
             uint8_t read[8];
@@ -113,42 +208,102 @@ void app_main(void)
             uint16_t temp =     (read[4] << 8) | read[5];
             uint16_t humid =    (read[2] << 8) | read[3];
 
-            printf("Temp: %.2f \n", (float)temp / 10);
-            printf("Humid: %.2f%% \n", (float)humid /10);
-
             // CRC check
-            unsigned short crc16(unsigned char *ptr, unsigned char len)
-            {
-                unsigned short crc =0xFFFF;
-                unsigned char i;
+            unsigned short crc_code = crc16(read, 6);
+            unsigned short crc_calc = (read[7] << 8) + (read[6]);
 
-                while(len--)
-                {
-                    crc ^=*ptr++;
-                    for(i=0;i<8;i++)
-                    {
-                        if(crc & 0x01)
-                        {
-                        crc>>=1;
-                        crc^=0xA001;
-                        }else
-                        {
-                        crc>>=1;
-                        }
-                    }
+            // printf("crc calc: 0x%04X\n", crc_code);
+            // printf("crc revc: 0x%02X%02X\n", read[7], read[6]);
+
+            if (crc_code == crc_calc) {
+                printf("Temp: %.2f\n", (float)temp / 10);
+                // printf("Humid: %.2f%% \n", (float)humid /10);
+
+                // RGB
+                if (temp < 230) {
+                    // purple
+                    gpio_set_level(RED_LED, 0);
+                    gpio_set_level(GREEN_LED, 1);
+                    gpio_set_level(BLUE_LED, 0);
                 }
-                return crc;
+                else if (temp > 230 && temp < 240) {
+                    // green
+                    gpio_set_level(RED_LED, 1);
+                    gpio_set_level(GREEN_LED, 0);
+                    gpio_set_level(BLUE_LED, 1);
+                }
+                else if (temp > 240 && temp < 250) {
+                    // red
+                    gpio_set_level(RED_LED, 0);
+                    gpio_set_level(GREEN_LED, 1);
+                    gpio_set_level(BLUE_LED, 1);
+                }
+                else if (temp > 250) {
+                    // white
+                    gpio_set_level(RED_LED, 0);
+                    gpio_set_level(GREEN_LED, 0);
+                    gpio_set_level(BLUE_LED, 0);
+                }
             }
-
-            printf("crc calc: 0x%04X\n", crc16(read, 6));
-            printf("crc revc = 0x%02X%02X\n", read[7], read[6]);
         }
         
 
         if (!temp_humid_sensor) {
-            // Light readout
+            // Light sensor readout
 
+            // Printer den læste value fra photocell hvert halve sekund.
+            // Lav value = mørkt, høj value = lyst.
+            int adc_raw;
+            adc_oneshot_read(adc_handle, ADC_CHANNEL_2, &adc_raw);
+            printf("Value measured: %d\n", adc_raw);
         }
-        
     }
 }
+
+void red() {
+    gpio_set_level(RED_LED, 0);
+    gpio_set_level(GREEN_LED, 1);
+    gpio_set_level(BLUE_LED, 1);
+}
+
+void green() {
+    gpio_set_level(RED_LED, 1);
+    gpio_set_level(GREEN_LED, 0);
+    gpio_set_level(BLUE_LED, 1);
+}
+
+void blue() {
+    gpio_set_level(RED_LED, 1);
+    gpio_set_level(GREEN_LED, 1);
+    gpio_set_level(BLUE_LED, 0);
+}
+
+void purple() {
+    gpio_set_level(RED_LED, 0);
+    gpio_set_level(GREEN_LED, 1);
+    gpio_set_level(BLUE_LED, 0);
+}
+
+void yellow() {
+    gpio_set_level(RED_LED, 0);
+    gpio_set_level(GREEN_LED, 0);
+    gpio_set_level(BLUE_LED, 1);
+}
+
+void white() {
+    gpio_set_level(RED_LED, 0);
+    gpio_set_level(GREEN_LED, 0);
+    gpio_set_level(BLUE_LED, 0);
+}
+
+void cyan() {
+    gpio_set_level(RED_LED, 1);
+    gpio_set_level(GREEN_LED, 0);
+    gpio_set_level(BLUE_LED, 0);
+};
+
+void all_off() {
+    gpio_set_level(RED_LED, 1);
+    gpio_set_level(GREEN_LED, 1);
+    gpio_set_level(BLUE_LED, 1);
+};
